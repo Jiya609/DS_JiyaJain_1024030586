f44c7006afe66f2ecea7db1625893d938472c157
Jiya <jjiya6764@gmail.com>
2025-08-19 22:37:37 +0530

    Fourth question(b)

---
+++ Assgn1_q4(b).cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, n, p;
+    cout << "Enter rows and cols of first matrix: ";
+    cin >> m >> n;
+    cout << "Enter cols of second matrix: ";
+    cin >> p;
+
+    int A[10][10], B[10][10], C[10][10] = {0};
+
+    cout << "Enter elements of Matrix A: ";
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            cin >> A[i][j];
+
+    cout << "Enter elements of Matrix B: ";
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < p; j++)
+            cin >> B[i][j];
+
+    // Multiplication
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < n; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    cout << "Resultant Matrix (A x B):\n";
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++)
+            cout << C[i][j] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}

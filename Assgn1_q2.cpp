60d273992d1f1ff44325ef7c2681c8242b088a7c
Jiya <jjiya6764@gmail.com>
2025-08-21 10:27:01 +0530

    added file

--- Assgn1_q2.cpp
+++ Assgn1_q2.cpp
@@ -11,16 +11,16 @@ int main() {
     for (int i = 0; i < n; i++)
         cin >> arr[i];
 
-    // Removing duplicates
+    
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             if (arr[i] == arr[j]) {
-                // Shift left
+                
                 for (int k = j; k < n - 1; k++) {
                     arr[k] = arr[k + 1];
                 }
-                n--;   // reduce size
-                j--;   // recheck new element at j
+                n--;   
+                j--;   
             }
         }
     }
